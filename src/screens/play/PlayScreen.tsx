93ce1c1a8fb7848f1e757dc9ecab3e900addb0ef
Bowei Liu <liubowei@gmail.com>
2023-07-09 12:31:23 -0700

    wip measuring flex is hard

--- src/screens/play/PlayScreen.tsx
+++ src/screens/play/PlayScreen.tsx
@@ -6,48 +6,86 @@ export const GrayBorder = (props: { width, height, ChildComponent, borderWidth }
     const { width, height, ChildComponent, borderWidth = 6 } = props;
 
     return (<div className={styles.grayBorder} style={{ borderWidth: borderWidth + 'px' }}>
-      <ChildComponent width={width - 2  * borderWidth} height={height - 2  * borderWidth} />
+      <ChildComponent {...props} width={width - 2  * borderWidth} height={height - 2  * borderWidth} />
     </div>);
 }
 
 export const withGrayBorder = (ChildComponent) => {
-    return (props) => (<GrayBorder ChildComponent={ChildComponent} {...props} />)
+    return (props) => (<GrayBorder {...props} ChildComponent={ChildComponent} />)
 }
 
 export const Padding = (props: {width, height, ChildComponent, padding }) => {
-    const { width, height, ChildComponent, padding = 16 } = props;
+    const { width, height, ChildComponent, padding } = props;
 
     return (<div className={styles.withPadding} style={{ padding: padding + 'px' }}>
-      <ChildComponent width={width - 2  * padding} height={height - 2  * padding} />
+      <ChildComponent {...props} width={width - 2  * padding} height={height - 2  * padding} />
     </div>);
 }
 
-export const withPadding = (ChildComponent) => {
-    return (props) => (<Padding ChildComponent={ChildComponent} {...props} />);
+export const withPadding = (padding) => {
+    return ((ChildComponent) => {
+        return (props) => (<Padding {...props} padding={padding} ChildComponent={ChildComponent} />);
+    })
 }
 
-export const PlayScreen = withGrayBorder(withPadding(MyPlayScreen));
+// aspectRatio = width / height
+export const AutoWidth = (props: {width, height, ChildComponent, aspectRatio }) => {
+    const { width, height, ChildComponent, aspectRatio = 4./7 } = props;
+
+    return (<ChildComponent {...props} height={height} width={height * aspectRatio} />)
+}
+
+export const withAutoWidth = (ChildComponent) => {
+    return (props) => (<AutoWidth {...props} ChildComponent={ChildComponent} />);
+}
+
+
+// Reading in order, goes outside in. so for instance here we apply gray border then apply padding.
+export const PlayScreen = withGrayBorder(withPadding(16)(MyPlayScreen));
 
 export function MyPlayScreen(props: { width: number, height: number }) {
   const { width, height } = props;
   return (
     <div className={styles.playScreenContainer} >
-        <div className={styles.rowsWrapper} style={{height}}>width {width}</div>
-        <div className={styles.singleInfoArea} style={{height, width: height/1.75}}>height {height}</div>
+        <div className={styles.rowsWrapper} style={{height}}>width {width} height {height}</div>
+        <BigCard width={width} height={height} />
     </div>
   );
 }
 
-function FixedAspectRatio(props: { children }) {
-    const { children  } = props;
+// const { rendered: renderedBigCard, width, height } = useMeasureRender(BigCard, { height });
+// ...
+// return (<> {renderedBigCard} </>)
 
-    return (
-        <div style={{position: 'absolute', height: '100%'}}>
-        <svg xmlns="http://www.w3.org/2000/svg" height="175" width="100" style={{ height: '100%', width: 'auto' }}/>
-        <div style={{ position: 'absolute', inset: 0, backgroundColor: 'red'}}>
-        {children}
-        </div>
-        </div>)
+// const RowsWrapperAndBigCard = withLeftRight({ padding: 16 })(MyRowsWrapper, BigCard);
+
+// export const withLeftRight = (args: { padding: number  }) => {
+//     const { padding  } = args;
+//     return (Child1, Child2) => {
+//         return (props: { width, height }) => {
+//             const { width, height } = props;
+//             return (<>
+//                 <Child1 {...props} height={height} width={}/>
+//                 <div width={padding} height={height} />
+//                 <Child2 {...props} />
+//                 </>);
+// 
+//         }
+//     }
+// }
+
+const MyRowsWrapper = (props: { width, height }) => {
+  const { width, height } = props;
+    return (<div className={styles.rowsWrapper} style={{width, height}}>width {width} height {height}</div>);
+}
+
+
+
+const BigCard = withPadding(12)(withAutoWidth(MyBigCard))
+
+function MyBigCard(props: { width, height }) {
+  const { width, height } = props;
+  return <div className={styles.singleInfoArea} style={{width, height}}>height {height} width {width}</div>
 }
 
 function PlayHud() {
