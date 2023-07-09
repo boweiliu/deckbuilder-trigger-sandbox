51e37064a407d0d69afdcf6d82f8a4306c99b03d
Bowei Liu <liubowei@gmail.com>
2023-07-09 04:43:36 -0700

    grr

--- src/screens/play/PlayScreen.tsx
+++ src/screens/play/PlayScreen.tsx
@@ -23,16 +23,7 @@ export function MyPlayScreen(props: { width: number, height: number }) {
   const { width, height  } = props;
   return (
     <div className={styles.playScreenContainer}>
-      <div style={{ width: 'calc(100% - 8px)', height: 'calc(35% - 8px)', backgroundColor: '#333', display: 'flex', padding: '4px', border: 'solid 2px #ccc', borderRadius: '6px', margin: '4px', 'overflowX': 'auto',   'overscrollBehavior': 'contain',  'scrollbarWidth': 'none', }} id="cards-container">
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', 'marginLeft': 'auto', }}>card 1</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', }}>card 2</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', }}>card 4</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', }}>card 5</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', }}>card 5</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', }}>card 5</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', }}>card 6</div>
-        <div style={{ margin: '4px', backgroundColor: 'blue', minWidth: '150px', 'marginRight': 'auto', }}>card 3</div>
-      </div>
+      <div>hello</div>
     </div>
   );
 }
