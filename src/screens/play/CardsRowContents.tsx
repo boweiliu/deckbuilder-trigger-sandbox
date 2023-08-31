f6df5c81b4fadde8b1a8f3dfdc0074eda03596f0
Bowei Liu <liubowei@gmail.com>
2023-08-31 16:57:44 -0700

    wip more refactor to keep scrollers state

--- src/screens/play/CardsRowContents.tsx
+++ src/screens/play/CardsRowContents.tsx
@@ -11,20 +11,42 @@ import { Sizes } from '@/components/hoc/sizing';
 
 import { SmallCard } from '@/screens/play/PlayScreen';
 
-export function CardsRowContents(props: Sizes & { count: number }) {
-  const { height, width, count } = props;
+export function CardsRowContents(
+  props: Sizes & {
+    count: number;
+    setIsScrolledToLeft?: (b: boolean) => void;
+    setIsScrolledToRight?: (b: boolean) => void;
+  }
+) {
+  const { height, width, count, setIsScrolledToLeft, setIsScrolledToRight } =
+    props;
   const ref = useRef<HTMLDivElement | null>(null);
 
-  const onWheelScroll = useCallback((e: React.WheelEvent<HTMLDivElement>) => {
+  const afterScroll = useCallback(() => {
     if (ref.current) {
-      ref.current.scrollLeft += e.deltaY;
+      const el = ref.current;
+      setIsScrolledToLeft?.(el.scrollLeft === 0);
+      setIsScrolledToRight?.(
+        Math.ceil(el.scrollWidth - el.scrollLeft) === el.clientWidth
+      );
     }
-  }, []);
+  }, [setIsScrolledToLeft, setIsScrolledToRight]);
+
+  const onWheelScroll = useCallback(
+    (e: React.WheelEvent<HTMLDivElement>) => {
+      if (ref.current) {
+        ref.current.scrollLeft += e.deltaY;
+        afterScroll();
+      }
+    },
+    [afterScroll]
+  );
 
   return (
     <div
       ref={ref}
       onWheel={onWheelScroll}
+      onScroll={afterScroll}
       className={styles.rowContents}
       style={{
         height:
@@ -36,5 +58,43 @@ export function CardsRowContents(props: Sizes & { count: number }) {
         <SmallCard key={idx} width={Infinity} height={height} />
       ))}
     </div>
+  );
+}
+
+export function CardsRowScrollable(
+  props: Sizes & { borderRadius: number; count: number }
+) {
+  const { width, height, borderRadius: innerBorderRadius, count } = props;
+
+  const shouldShowScrollers = true;
+
+  return (
+    <>
+      {shouldShowScrollers && (
+        <div
+          className={styles.rowLeftScroller}
+          style={{
+            width: height * 0.22,
+            fontSize: height * 0.15,
+            borderRadius: `${innerBorderRadius}px 0px 0px ${innerBorderRadius}px`,
+          }}
+        >
+          {'<'}
+        </div>
+      )}
+      <CardsRowContents width={width} height={height} count={count} />
+      {shouldShowScrollers && (
+        <div
+          className={styles.rowRightScroller}
+          style={{
+            width: height * 0.22,
+            fontSize: height * 0.15,
+            borderRadius: `0px ${innerBorderRadius}px ${innerBorderRadius}px 0px`,
+          }}
+        >
+          {'>'}
+        </div>
+      )}
+    </>
   );
 }
